78054d9953e335718e4baf1a1ccfae04f0461f65
Declan-101 <wildedeclan5@gmail.com>
2026-05-29 20:40:49 -0700

    added code verification system

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -1,31 +1,51 @@
 /* ------ RANDOM CODES ------ */
+// NOTE: submit button is initially disabled upon loading
 
-//Function to generate combination of characters
+var code = "";
+var getCode = "";
+var btnvalue;
+var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@$";
+
+// Function to generate combination of characters
 function generateCode() {
-
-    //Create variables
-    var code = "";
-    var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@$";
-
-    //Generate character multiple times using a loop
-    for (i = 1; i <= 8; i++) {
-
-        var char = Math.random() * str.length;
-
+    code = "";
+    for (var i = 1; i <= 8; i++) {
+        var char = Math.floor(Math.random() * str.length);
         code += str.charAt(char);
     }
-
     return code;
 }
 
-//Get HTML element to display
+// Display generated code
 document.getElementById("codes").innerHTML = generateCode();
 
-//Disable Button
-function disableButton() {
-
-    document.getElementById("submit").disabled = true;
+// Determine when to enable or disable button
+function disableButton(btnvalue) {
+    document.getElementById("submit").disabled = btnvalue;
+    if (btnvalue == true) {
+        document.getElementById("submit").style.backgroundColor =
+            "rgba(73,119,209,0.3)";
+        document.getElementById("submit").style.color =
+            "rgba(255,255,255,0.5)";
+    } else {
+        document.getElementById("submit").style.backgroundColor =
+            "rgba(73,119,209,1)";
+        document.getElementById("submit").style.color =
+            "rgba(255,255,255,1)";
+    }
 }
 
-//Activate function
-disableButton();
+// Listen for user input
+var codebox = document.getElementById("codeentered");
+codebox.addEventListener("input", evaluateCode);
+
+// Run function when user types
+function evaluateCode() {
+    getCode = document.getElementById("codeentered").value;
+    var charset1 = getCode.trim();
+    var charset2 = code.trim();
+    if (charset1.length == charset2.length &&
+        charset1 == charset2) {
+        disableButton(false);
+    }
+}
