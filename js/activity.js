61b506321c3d0eedb5753ff73b77328e0636e158
Declan-101 <wildedeclan5@gmail.com>
2026-06-04 19:48:29 -0700

    homework for week5 day2

--- js/activity.js
+++ js/activity.js
@@ -10,10 +10,37 @@ $(document).ready(function () {
 
     $("tbody td").click(function () {
 
-        if ($(this).text() !== "Not Available" && !$(this).is(":first-child")) {
-            $(this).toggleClass("selected");
-        }
+    var content = $(this).text();
 
-    });
+    if (content !== "Not Available" && !$(this).is(":first-child")) {
+
+        $(this).toggleClass("selected");
+
+        if ($(this).hasClass("selected")) {
+
+            $("#displaySelected").css("visibility", "visible");
+            $("#displaySelected").css("margin-top", "2em");
+
+            var colIndex = $(this).index();
+
+var cliffName = $("thead th").eq(colIndex).text();
+
+$("#result").append(
+    "<p>" + content + " at " + cliffName + "</p>"
+);
+
+        } else {
+
+            $("#result p:contains('" + content + "')").remove();
+
+            if ($("#result").has("p").length == false) {
+
+                $("#displaySelected").css("visibility", "hidden");
+                $("#displaySelected").css("margin-top", "0");
+            }
+        }
+    }
+
+});
 
 });
